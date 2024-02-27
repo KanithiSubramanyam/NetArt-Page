760fbe96ab3b2a25d8edf2aaa1e79e316ab7796b
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-02-27 23:19:36 +0530

    webpage completed

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -76,15 +76,30 @@ const Page = () => {
             </p>
           </div>
         </div>
+        <div className="Line"></div>
         <div className="Last-Content">
           <div className="L-Head">
             C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
           </div>
           <div className="L-List">
-            CHEMICALS & PROCESS POWER WATER & WASTE WATER OILS & GAS PHARMA
-            SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING
-            FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE
-            FIGHTING AGRICULTURE & RESIDENTIAL
+            <ul>
+                <li>CHEMICALS & PROCESS</li>
+                <li>POWER</li>
+                <li>WATER & WASTE WATER</li>
+                <li>OILS & GAS</li>
+                <li>PHARMA</li>
+                <li> SUGARS & DISTILLERIES</li>
+                <li>PAPER & PULP</li>
+                <li>MARINE & DEFENCE</li>
+                <li> METAL & MINING</li>
+                <li>FOOD & BEVERAGE</li>
+                <li>PETROCHEMICAL & REFINERIES</li>
+                <li>SOLAR</li>
+                <li>BUILDING</li>
+                <li>HVAC</li>
+                <li>FIRE FIGHTING</li>
+                <li>AGRICULTURE & RESIDENTIAL</li>
+            </ul>
           </div>
         </div>
         <div className="Footer">
